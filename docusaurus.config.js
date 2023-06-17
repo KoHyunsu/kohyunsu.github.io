5fbeaedd36ccf5443a4980a1135b28bdcd24c34e
KoHyunsu <39062454+KoHyunsu@users.noreply.github.com>
2023-06-17 21:10:31 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,6 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'KoHyunsu', // Usually your GitHub org/user name.
   projectName: 'kohyunsu.github.io', // Usually your repo name.
+  deploymentBranch: 'main',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
